63f77d516752a982c395eaf451f328385379bd15
Jay Wright <kwharty@gmail.com>
2019-01-11 21:11:38 -0800

    comp update

--- monkey/src/App.js
+++ monkey/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import MonkCard from "./components/MonkCard";
